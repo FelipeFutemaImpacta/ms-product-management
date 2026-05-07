34953b719b663c068592cdd385de95f5c89cfecf
Felipe Futema <futema09@gmail.com>
2026-05-07 18:57:10 -0300

    commit inicial

---
+++ src/main/java/br/com/impacta/lab/MsProductManagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.impacta.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProductManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsProductManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/impacta/lab/MsProductManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.impacta.lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsProductManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
